f9a11cb1f94061e83f0794a7ed9d89e3289837e1
Marta Roncero <m.ronceroolmeda@gmail.com>
2021-03-02 17:12:47 +0000

    Created donate page with route and links fromn homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :about]
+  skip_before_action :authenticate_user!, only: [ :home, :about, :donate]
 
   def home
   end
 
   def about
+  end
+
+  def donate
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/about' => 'pages#about'
+  get '/donate' => 'pages#donate'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
