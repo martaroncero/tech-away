e5587d1e482f3c2ca45ab1b034af5ffabb6f2ce4
Marta Roncero <m.ronceroolmeda@gmail.com>
2021-03-02 13:16:26 +0000

    Added update method in prodcts

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :products, only: [:index, :show, :new, :create, :edit] do
+  resources :products, only: [:index, :show, :new, :create, :edit, :update] do
      resources :bookings, only: [:create]
   end
 
