22f888bbba057da1839e3e55e27c56e583238910
Won Lee <cathykw2018@gmail.com>
2021-03-02 16:35:01 +0000

    booking features

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,7 @@
 class BookingsController < ApplicationController
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.product = @product
+    @booking.user = current_user
+  end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
+  def show
+    @booking = Booking.new
+    @product = Product.find(params[:id])
+  end
+
+  def index
+     @product = Product.all
+  end
+
 end
