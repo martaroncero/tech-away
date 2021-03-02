600af84bb2a363bdc998275baa4cae14cb5c22b5
Ryan Woods <contact@ryanofwoods.com>
2021-03-02 15:26:37 +0100

    added necessary actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+
+  end
+  
+  def show
+
+  end
+
+  def index
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+
+  def set_user
+
+  end
+
+  def user_params
+
+  end
 end
