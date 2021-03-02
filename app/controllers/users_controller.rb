5533410d8c0e5df4195618062036c1892676ad1f
Ryan Woods <contact@ryanofwoods.com>
2021-03-02 15:46:07 +0100

    add new and create method and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
+    raise
   end
 
   def create
-
+    @user = User.new(user_params)
   end
   
   def show
@@ -30,6 +32,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-
+    params.require(:user).permit(:email, :password, :first_name, :last_name, :description)
   end
 end
