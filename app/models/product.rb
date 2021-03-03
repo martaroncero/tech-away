57b783aeb830c74c099a05953db8181f188a9fc1
Ryan Woods <contact@ryanofwoods.com>
2021-03-03 17:56:26 +0100

    add a pg search for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,13 @@ class Product < ApplicationRecord
   
   CONDITIONS = ["New", "Used (like new)", "Used (good)", "Used (fair)"]
   validates :condition, inclusion: {in: CONDITIONS }
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title_description_and_category,
+    against: [ :title, :description ],
+    associated_against: {
+      category: [ :title ]
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
