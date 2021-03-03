10fbda6f4ea7da308e8ca1cba46d665935e0c7a7
Ryan Woods <contact@ryanofwoods.com>
2021-03-03 12:50:53 +0100

    Add request model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_03_114014) do
+ActiveRecord::Schema.define(version: 2021_03_03_114943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,15 @@ ActiveRecord::Schema.define(version: 2021_03_03_114014) do
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
+  create_table "requests", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_requests_on_category_id"
+    t.index ["user_id"], name: "index_requests_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -80,5 +89,7 @@ ActiveRecord::Schema.define(version: 2021_03_03_114014) do
   add_foreign_key "bookings", "users"
   add_foreign_key "products", "categories"
   add_foreign_key "products", "users"
+  add_foreign_key "requests", "categories"
+  add_foreign_key "requests", "users"
   add_foreign_key "users", "charities"
 end
