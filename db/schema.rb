3b03f022e31ec1900375d85d7efd5244ff1b5799
Ryan Woods <contact@ryanofwoods.com>
2021-03-03 12:39:06 +0100

    generate category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_120158) do
+ActiveRecord::Schema.define(version: 2021_03_03_113813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 2021_03_02_120158) do
     t.index ["charity_id"], name: "index_bookings_on_charity_id"
     t.index ["product_id"], name: "index_bookings_on_product_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "charities", force: :cascade do |t|
@@ -57,7 +63,7 @@ ActiveRecord::Schema.define(version: 2021_03_02_120158) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "kind"
+    t.string "kind", default: "Donator"
     t.text "description"
     t.string "phone_number"
     t.string "contact_preference"
