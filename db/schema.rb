0cd28d7c10515f7e335b117d166281e428f39a9e
Ryan Woods <contact@ryanofwoods.com>
2021-03-03 17:06:21 +0100

    add coordinates to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_03_124004) do
+ActiveRecord::Schema.define(version: 2021_03_03_160527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 2021_03_03_124004) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "category_id", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
