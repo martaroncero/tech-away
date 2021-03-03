a88c23a7847f11dc5a94ca8c7cdefe366b077d67
Ryan Woods <contact@ryanofwoods.com>
2021-03-03 13:03:16 +0100

    remove incorrect validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,4 @@
 class Category < ApplicationRecord
-
-  CATEGORIES = [ "Camera", "Desktop", "Laptop", "Mobile Phone", "Monitor", "Printer" ]
-
-  validates :title, inclusion: { in: CATEGORIES }
-
   has_many :products
   has_many :requests
 end
