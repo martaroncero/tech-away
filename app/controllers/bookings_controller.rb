61f108ee48a0a6c625b34d5119d1f4eb0dcb452d
Won Lee <cathykw2018@gmail.com>
2021-03-03 12:37:50 +0000

    edits

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,21 +1,23 @@
 class BookingsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
-    @booking = Booking.new(booking_params)
+    @booking = Booking.new
     @booking.product = @product
-    @booking.user = User.where(charity: current_user.charity, kind: "Seeker")
+    @booking.user = User.where(charity: current_user.charity, kind: "Seeker").first
     @booking.charity = current_user.charity
     @booking.save
     redirect_to bookings_path
   end
 
-  # def booking_params
-  #   params.require(:booking).permit(:status)
-  # end
 
   def index
     @user = current_user
     @bookings = @user.bookings.where(charity_id: @user.charity_id)
+  end
+
+private
+  def booking_params
+    params.require(:booking).permit(:status)
   end
 
 end
