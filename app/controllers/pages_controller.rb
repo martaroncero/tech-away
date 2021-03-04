4b6ffea3e302d15c8f0d058a8dba12d53127ff1f
Ryan Woods <contact@ryanofwoods.com>
2021-03-04 11:44:26 +0100

    add request class method for count per category

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :about, :donate]
 
   def home
+    @requests_count = Request.count_per_category(1)
+    raise
   end
 
   def about
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,8 @@
 class Request < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  def self.count_per_category(*limit)
+    Category.select('categories.id, categories.title, count(request.id) as count').includes("requests")
+  end
 end
