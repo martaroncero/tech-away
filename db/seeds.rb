52ceb48e782a42f37e7a6956f2e79172f6138a55
gayatriarora <gayatri7arora@gmail.com>
2021-03-09 08:30:49 +0000

    add destroy all before creating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,13 @@ require 'open-uri'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 puts "Creating seeds..."
+Category.destroy_all
+Charity.destroy_all
+User.destroy_all
+Product.destroy_all
+Request.destroy_all
+Booking.destroy_all
+
 
 #Categories
 category_camera = Category.create!(title: "Camera")
