1cd360186def02410f3984c825f27429410fc02b
gayatriarora <gayatri7arora@gmail.com>
2021-03-09 09:46:25 +0000

    added more images

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,6 @@ require 'open-uri'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 puts "Creating seeds..."
-Category.destroy_all
-Charity.destroy_all
-User.destroy_all
-Product.destroy_all
-Request.destroy_all
-Booking.destroy_all
-
 
 #Categories
 category_camera = Category.create!(title: "Camera")
@@ -18,7 +11,7 @@ category_tablet = Category.create!(title: "Tablet")
 category_mobile = Category.create!(title: "Mobile phone")
 category_printer = Category.create!(title: "Printer")
 category_monitor = Category.create!(title: "Monitor")
-
+category_camera = Category.create!(title: "Camera")
 
 # Charities
 charity1 = Charity.create!(name: "Refugee aid", 
@@ -29,18 +22,6 @@ charity2 = Charity.create!(name: "We are one",
                           website: "www.weareone.org", 
                           whatsapp_number: "+4430712929"
                           )
-charity3 = Charity.create!(name: "Refugee-Voice", 
-                          website: "www.refugee-voice.org", 
-                          whatsapp_number: "+44307129760"
-                          )
-charity4 = Charity.create!(name: "She Refugee aid", 
-                          website: "www.sherefugeeaid.com", 
-                          whatsapp_number: "+4465712955"
-                          )
-charity5 = Charity.create!(name: "World for all", 
-                          website: "www.world-for-all.uk.org", 
-                          whatsapp_number: "+446572115"
-                          )
 
 #Users
 user_donator = User.create!(email: "donator@mail.com", 
@@ -49,109 +30,47 @@ first_name: "Nigel",
 last_name: "Lee", 
 kind: "Donator", phone_number: "+441234120", contact_preference: "Either")
 
-user_seeker_1 = User.new(email: "seeker1@mail.com",
+user_seeker_1 = User.new(email: "seeker@mail.com",
                         password: "123456",
                         first_name: "Amal",
                         last_name: "Shaw",
-                        charity_id: charity1.id,
+                        charity_id: charity2.id,
                         kind: "Seeker",
-                        description: "Before she was forced to flee civil war in Syria, Amal was studying to be lawyer. She needs to pass an advanced English exam to get into university, and needs a laptop for the same. Amal hopes she can continue her education here in Britain and someday be a successful lawyer.",
+                        description: "Before she was forced to flee civil war in Sudan, Amal was studying to be lawyer. She needs to pass an advanced English exam to get into university. Amal hopes she can continue her education here in Britain and someday be a successful lawyer.",
                         phone_number: "+443339200")
 user_seeker_1.photo.attach(io: URI.open('https://res.cloudinary.com/ds32iiece/image/upload/v1614945795/photo-1583431978096-e09dc7b7fcf4_ojjhlr.jpg'),
-                          filename: "user_seeker_1.jpg",
+                          filename: "user_2.jpg",
                           content_type: "image/jpg")
 user_seeker_1.save!
 
-user_seeker_2 = User.new(email: "seeker2@mail.com",
-                        password: "123456" ,first_name: "Saloo",
+user_seeker_2 = User.new(email: "seeker1@mail.com",
+                        password: "123456",
+                        first_name: "Saloo",
                         last_name: "Mir",
                         charity_id: charity2.id,
                         kind: "Seeker",
-                        description: "Saloo is from Bhutan and was studying in university before he had to seek asylum in Britain.He needs a laptop to study social work so that he can work with his local council to support other young refugees .",
-                        phone_number: "+44333920")
+                        description: "Saloo was a student back in Syria, before his family had to flee. He aspires to be a software engineer.",
+                        phone_number: "+44333920"
+                        )
 user_seeker_2.photo.attach(io: URI.open('https://res.cloudinary.com/ds32iiece/image/upload/v1614945726/download_irxjsm.jpg'),
-                          filename: "user_seeker_2.jpg",
+                          filename: "user_s1.jpg",
                           content_type: "image/jpg"
                           )
 user_seeker_2.save!
 
-user_seeker_3 = User.new(email: "seeker3@mail.com",
+user_seeker_3 = User.new(email: "seeker2@mail.com",
                         password: "123456"  ,first_name: "Bob",
                         last_name: "Ameer",
-                        charity_id: charity3.id,
-                        kind: "Seeker",
-                        description: "Bob was studying in his final year of university, before his family had to flee. He is a self taught programmer and would like a desktop to continue his studies. Bob aspires to be a software engineer.",
-                        phone_number: "+44333920")
-user_seeker_3.photo.attach(io: URI.open('https://res.cloudinary.com/ds32iiece/image/upload/v1614945729/photo-1562925217-61a965391d53_x7pnfh.jpg'),
-                          filename: "user_seeker_3.jpg",
-                          content_type: "image/jpg"
-                          )
-user_seeker_3.save!
-
-user_seeker_4 = User.new(email: "seeker4@mail.com",
-                        password: "123456"  ,first_name: "Abebe",
-                        last_name: "Lea",
                         charity_id: charity2.id,
                         kind: "Seeker",
-                        description: "Originally from Ethiopia, Abebe fled Kenya with his wife and new born, leaving everything behind. He was a photographer and worked for the local newspaper but had to leave everything behind. He needs a camera so that he can once again stand up on his feet, support his family and start a new life here in Britain.",
-                        phone_number: "+443992200")
-user_seeker_4.photo.attach(io: URI.open('https://res.cloudinary.com/ds32iiece/image/upload/v1615203342/photo-1529688530647-93a6e1916f5f_wxxbbf.jpg'),
+                        description: "Bob was studying photography in university before he had to seek asylum in Britain.",
+                        phone_number: "+44333920"
+                        )
+user_seeker_3.photo.attach(io: URI.open('https://res.cloudinary.com/ds32iiece/image/upload/v1614945729/photo-1562925217-61a965391d53_x7pnfh.jpg'),
                           filename: "user_s2.jpg",
                           content_type: "image/jpg"
                           )
-user_seeker_4.save!
-
-user_seeker_5 = User.new(email: "seeker5@mail.com",
-                        password: "123456"  ,first_name: "Noor",
-                        last_name: "Ehsan",
-                        charity_id: charity4.id,
-                        kind: "Seeker",
-                        description: "Noor was forced to leave her husband in Syria and seek asylum in Britain for the sake of her young daughter's safety. All alone here in Britain, Noor would like a mobile phone so she can start her own catering business and also stay in contact with her husband who is still in Syria, waiting to speak and see his wife and daughter.",
-                        phone_number: "+443992200")
-user_seeker_5.photo.attach(io: URI.open('https://res.cloudinary.com/ds32iiece/image/upload/v1615204589/download_j5oj6t.jpg'),
-                          filename: "user_seeker_5.jpg",
-                          content_type: "image/jpg"
-                          )
-user_seeker_5.save!
-
-user_seeker_6 = User.new(email: "seeker6@mail.com",
-                        password: "123456"  ,first_name: "Zaida",
-                        last_name: "Zen",
-                        charity_id: charity4.id,
-                        kind: "Seeker",
-                        description: "Zaida, a young mother was studying psychology at university and was forced to leave war torn Iran with her baby. She needs a laptop to continue her studies and finish her degree in psychology. Zaida aspires to be a councellor so that she can support other refugees and young mothers.",
-                        phone_number: "+443911231")
-user_seeker_6.photo.attach(io: URI.open('https://res.cloudinary.com/ds32iiece/image/upload/v1615204589/download_1_x2zik8.jpg'),
-                          filename: "user_seeker_6.jpg",
-                          content_type: "image/jpg"
-                          )
-user_seeker_6.save!
-
-user_seeker_7 = User.new(email: "seeker7@mail.com",
-                        password: "123456"  ,first_name: "Sulaiman",
-                        last_name: "Sair",
-                        charity_id: charity5.id,
-                        kind: "Seeker",
-                        description: "Sulaiman, an engineer fled Syria with his mother and endured a five-hour sea crossing to Europe on a dangerous overcrowded boat. He arrived with nothing but aspirations to start a new life. He needs a desktop and a printer so that he can pass an advance english exam to be able to look for a job as an engineer here in London.",
-                        phone_number: "+444211540")
-user_seeker_7.photo.attach(io: URI.open('https://res.cloudinary.com/ds32iiece/image/upload/v1615212959/download_2_xknqu1.jpg'),
-                          filename: "user_seeker_7.jpg",
-                          content_type: "image/jpg"
-                          )
-user_seeker_7.save!
-
-user_seeker_8 = User.new(email: "seeker8@mail.com",
-                        password: "123456"  ,first_name: "Wafaa",
-                        last_name: "Jog",
-                        charity_id: charity1.id,
-                        kind: "Seeker",
-                        description: "Wafaa was in her final year of university studying International Finance , before she was forced to flee the civil war in Libya and seek asylum in Britain. She needs a laptop to continue her studies so that she can be independent and secure a bright future.",
-                        phone_number: "+44428840")
-user_seeker_8.photo.attach(io: URI.open('https://res.cloudinary.com/ds32iiece/image/upload/v1614945795/photo-1583431978096-e09dc7b7fcf4_ojjhlr.jpg'),
-                          filename: "user_seeker_8.jpg",
-                          content_type: "image/jpg"
-                          )
-user_seeker_8.save!
+user_seeker_3.save!
 
 user_worker = User.create!(email: "worker@mail.com",
                           password: "123456",
@@ -165,7 +84,7 @@ user_worker = User.create!(email: "worker@mail.com",
 #Products 
 product_dell_laptop = Product.new(user_id: user_donator.id,
                       title: "Dell Laptop",
-                      description: " Dell Latitude E7470 . This laptop is 14inches,  6th Gen with i5 processor. It has pre-installed Windows 10 operating system and 8GB RAM.",
+                      description: " Dell Lauser_id: user_donator.id, titude E7470 . This laptop is 14inches,  6th Gen with i5 processor. It has pre-installed Windows 10 operating system  and 8GB RAM.",
                       status: "Pending",
                       condition: "Used (fair)",
                       category_id: category_laptop.id,
@@ -221,7 +140,7 @@ product_desktop = Product.create!(user_id: User.last.id,
                           title: "Desktop", 
                           description: "HP Pavilion 24-xa Core i3 Windows 10 Desktop. The desktop is in pristine condition, brought for my son but was used only for a few months.", 
                           status: "Pending", 
-                          condition: "Used (good)",category_id: category_desktop.id, 
+                          condition: "Used (good)",category_id: Category.last.id, 
                           address: "Charteris Road, London NW6"
                           )
 product_desktop.photo.attach(io: URI.open('https://res.cloudinary.com/ds32iiece/image/upload/v1614948906/download_2_zip61w.jpg'), 
@@ -230,41 +149,15 @@ product_desktop.photo.attach(io: URI.open('https://res.cloudinary.com/ds32iiece/
                             )
 product_desktop.save!
 
-product_printer = Product.create!(user_id: User.last.id, 
-                          title: "HP Printer", 
-                          description: "This HP printer is in great condition, used for 6 months only.", 
-                          status: "Pending", 
-                          condition: "Used (good)",category_id: category_printer.id, 
-                          address: "Alma Road, St.Albans AL1"
-                          )
-product_printer.photo.attach(io: URI.open('https://res.cloudinary.com/ds32iiece/image/upload/v1614948906/download_2_zip61w.jpg'), 
-                            filename: 'printer.jpg', 
-                            content_type: 'image/jpg'
-                            )
-product_printer.save!
-
-product_monitor = Product.create!(user_id: User.last.id, 
-                          title: "Monitor 15inches", 
-                          description: "15inches Touch Screen LCD Monitor Display 1024x768 Resolution. It is 2 years old but used with care, minor scratches but in good usable condition.", 
-                          status: "Pending", 
-                          condition: "Used (good)",category_id: category_monitor.id, 
-                          address: "Winchester House, Merchant Street, Bow, Mile End, London E3"
-                          )
-product_monitor.photo.attach(io: URI.open('https://res.cloudinary.com/ds32iiece/image/upload/v1614948906/download_2_zip61w.jpg'), 
-                            filename: 'monitor.jpg', 
-                            content_type: 'image/jpg'
-                            )
-product_monitor.save!
-
 #Requests
-Request.create!(user_id: user_seeker_1.id, category_id: category_laptop.id)
-Request.create!(user_id: user_seeker_5.id, category_id: category_mobile.id)
-Request.create!(user_id: user_seeker_1.id, category_id: category_printer.id)
-Request.create!(user_id: user_seeker_3.id, category_id: category_desktop.id)
-Request.create!(user_id: user_seeker_3.id, category_id: category_monitor.id)
-Request.create!(user_id: user_seeker_2.id, category_id: category_printer.id)
-Request.create!(user_id: user_seeker_4.id, category_id: category_camera.id)
-Request.create!(user_id: user_seeker_3.id, category_id: category_tablet.id)
+Request.create!(user_id: user_seeker_1.id, category_id: category_laptop.id, description: "Amal needs a laptop for her studies.")
+Request.create!(user_id: user_seeker_1.id, category_id: category_mobile.id, description: "Amal needs a mobile for communication with fellow students")
+Request.create!(user_id: user_seeker_1.id, category_id: category_printer.id, description: "Amal needs a printer for printing university essays")
+Request.create!(user_id: user_seeker_2.id, category_id: category_desktop.id, description: "Needs a desktop for his software development")
+Request.create!(user_id: user_seeker_2.id, category_id: category_monitor.id, description: "Needs a monitor for his software development")
+Request.create!(user_id: user_seeker_2.id, category_id: category_printer.id, description: "Needs a printer for studying")
+Request.create!(user_id: user_seeker_3.id, category_id: category_camera.id, description: "Needs a camera so he can continue with his photography work")
+Request.create!(user_id: user_seeker_3.id, category_id: category_tablet.id, description: "Needs a tablet for editing his photography")
 
 #bookings
 Booking.create!(charity_id: charity2.id, 
@@ -273,7 +166,7 @@ Booking.create!(charity_id: charity2.id,
                 status: "Declined"
                 )
 Booking.create!(charity_id: charity2.id, 
-                user_id: user_seeker_2.id, 
+                user_id: user_seeker_1.id, 
                 product_id: product_macbook.id, 
                 status: "Accepted"
                 )
@@ -288,13 +181,13 @@ Booking.create!(charity_id: charity2.id,
                 status: "Pending"
                 )
 Booking.create!(charity_id: charity2.id, 
-                user_id: user_seeker_1.id, 
+                user_id: user_seeker_3.id, 
                 product_id: product_ipad.id, 
                 status: "Accepted"
                 )
 puts "Created seeds"
-puts "- 8 Users (1 Donator, 1 Worker and 7 Seekers)"
+puts "- 5 Users (1 Donator, 1 Worker and 3 Seekers)"
 puts "- 8 Categories"
 puts "- 8 Requests"
-puts "- 7 Products"
+puts "- 5 Products"
 puts "- 5 Bookings"
