214b72d520ab8c78c905e33e873ed945be7ffbcb
Ryan Woods <contact@ryanofwoods.com>
2021-03-10 13:56:42 +0100

    refactored the booking items

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -5,18 +5,11 @@ class CharitiesController < ApplicationController
   end
 
   def show
-    @user = User.all
-    @request = Request.find(params[:id])
-    @user_id = User.find(params[:id])
+    @user = User.new
     @charity = Charity.find(params[:id])
-    @seekers = @user.all.select{ |user| user.kind == "Seeker" }
-    @our_seekers = @charity.users.select{ |user| user.kind == "Seeker" }
-    @requests = @charity.requests
-    @pending_offers = @charity.bookings.where(user_id: @user, status: "Pending")
-    @accepted_offers = @charity.bookings.where(user_id: @user, status: "Accepted")
-    @declined_offers = @charity.bookings.where(user_id: @user, status: "Declined")
+    @charity_bookings = @charity.bookings
+    @our_seekers = User.where(charity: @charity, kind: "Seeker")
   end
-
 
   private
 
