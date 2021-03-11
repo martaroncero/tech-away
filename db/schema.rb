4d7d49e1b1ebc3d7bc780911eecbd75f7ad52e7b
Won Lee <cathykw2018@gmail.com>
2021-03-11 11:09:01 +0000

    removed description 2 field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_154720) do
+ActiveRecord::Schema.define(version: 2021_03_11_110418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 2021_03_10_154720) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
-    t.text "description2"
   end
 
   create_table "products", force: :cascade do |t|
