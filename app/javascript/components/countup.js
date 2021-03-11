8fdd9b73b90638dded3914a890e9db74eb500b1e
Won Lee <cathykw2018@gmail.com>
2021-03-11 16:40:38 +0000

    made counter roll faster

--- app/javascript/components/countup.js
+++ app/javascript/components/countup.js
@@ -1,8 +1,8 @@
 
   // How long you want the animation to take, in ms
-  const animationDuration = 2000;
+  const animationDuration = 700;
   // Calculate how long each ‘frame’ should last if we want to update the animation 60 times per second
-  const frameDuration = 1000 / 60;
+  const frameDuration = 500 / 60;
   // Use that to calculate how many frames we need to complete the animation
   const totalFrames = Math.round( animationDuration / frameDuration );
   // An ease-out function that slows the count as it progresses
