96fbe5a79d8c6d104eb836b58e3337ed8b4ce031
Ryan Woods <contact@ryanofwoods.com>
2021-04-27 20:19:19 +0200

    refactor users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-
-  before_action :set_user, only: [ :show, :edit, :update ]
+  before_action :set_user, only: [:show, :edit, :update]
 
   def new
     @user = User.new
